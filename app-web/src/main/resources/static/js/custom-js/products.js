a6be8014384ffaffd644f5772814a1c04643a8fb
NguyenAnhTuanE <nguyen.anh.tuane@sun-asterisk.com>
2020-05-11 16:41:57 +0700

    change page size

--- app-web/src/main/resources/static/js/custom-js/products.js
+++ app-web/src/main/resources/static/js/custom-js/products.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
     window.onload = function () {
         changeBackgroundMenuCategory();
     }
+    changePageSize();
 });
 
 function getUrlParamCategory() {
@@ -24,5 +25,42 @@ function changeBackgroundMenuCategory() {
             }
         });
 
+    }
+}
+
+function changePageSize() {
+    $(".page-size").on('change', function () {
+        let currentPageSize = getUrlParameter('size')
+
+        let newPageSize = $('.page-size').val();
+        let url = window.location.href;
+        if (currentPageSize) {
+            url = url.replace("size=" + currentPageSize, "size=" + newPageSize);
+        } else {
+            if (url.includes('=')){
+                url += "&size=" + newPageSize;
+            }else {
+                url += "?size=" + newPageSize;
+            }
+
+        }
+
+        window.location = url;
+    })
+
+}
+
+function getUrlParameter(sParam) {
+    let sPageURL = window.location.search.substring(1),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+        }
     }
 }
