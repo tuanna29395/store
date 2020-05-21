0bd5ccbbe4d4eea272e6f4e08d79caf2eed02f0f
NguyenAnhTuanE <nguyen.anh.tuane@sun-asterisk.com>
2020-05-21 17:18:05 +0700

    fix some bug

--- app-web/src/main/java/com/anhtuan/store/controller/HomeController.java
+++ app-web/src/main/java/com/anhtuan/store/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.anhtuan.store.controller;
 
+import com.anhtuan.store.commons.constants.Commons;
 import com.anhtuan.store.commons.constants.ModelViewConst;
 import com.anhtuan.store.commons.constants.ViewHtmlConst;
 import com.anhtuan.store.dto.request.UserRegisterRqDto;
@@ -8,12 +9,19 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class HomeController {
 
     @GetMapping("/login")
-    public String login(Model model) {
+    public String login(HttpServletRequest request, Model model, HttpSession session) {
         model.addAttribute(ModelViewConst.User.USER_REGISTER_DTO, new UserRegisterRqDto());
+        String referrer = request.getHeader("Referer");
+        if (!referrer.contains("login")){
+            session.setAttribute(Commons.SESSION_URL_PRIOR_LOGIN, referrer);
+        }
         return ViewHtmlConst.Users.SIGN_UP_SIGN_IN;
     }
 
