b971110e62fe3717135960c6f24ab12ae7e0a2a2
TuanE Nguyen Anh (tuanena-1734) <nguyen.anh.tuane@sun-asterisk.com>
2020-07-07 17:40:51 +0700

    fixxxx

--- app-web/src/main/resources/static/js/custom-js/product-detail.js
+++ app-web/src/main/resources/static/js/custom-js/product-detail.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
     renderSizeOption();
+    onclickSelectStar();
+    getReviews();
 });
 function renderSizeOption() {
     $.ajax({
@@ -17,3 +19,46 @@ function renderSizeOption() {
         }
     })
 }
+
+function onclickSelectStar() {
+    $('.number-star').on('click', function () {
+        let numberStar = $(this).data("star");
+
+        $(".your-rating").find(".number-star i").each(function () {
+            $(this).removeClass('color-red');
+            $(this).addClass('color-star');
+        });
+
+        $(this).find("i").removeClass('color-star');
+        $(this).find("i").addClass('color-red');
+        $("#star").val(numberStar);
+    })
+}
+function transformReviewItem(item) {
+    let data = {};
+    data.userName = item.user.username;
+    data.createdAt = moment(item.createdAt).format(DATE_PATTERN);
+
+    data.content = item.reviewContent;
+    data.numberStar = new Array(item.numberStar);
+
+    return data;
+}
+
+function getReviews() {
+    let productId = $("#product-id").val();
+    $.ajax({
+        url: `/review/${productId}/reviews`,
+        type: "GET",
+        async: false,
+        success: function (response) {
+            $(".review-content").empty();
+            response.content.forEach(function (item) {
+                let data = transformReviewItem(item)
+                $("#item-review").tmpl(data).appendTo(".review-content");
+            });
+            $("#number-review").text(response.content.length);
+        }
+    })
+}
+
